0ba3e8daa5b8120d524a8c2b0343dfcfa076b318
Beleks <53120159+Beleks@users.noreply.github.com>
2021-09-10 16:51:52 +0800

    addTime

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="header">
       <div class="place"></div>
-      <div class="date"></div>
+      <div class="date">{{ time.hour }}<span>:</span>{{ time.minutes }}</div>
     </div>
     <div class="current_weather"></div>
     <WeatherForTheDay />
@@ -27,7 +27,59 @@ export default {
   },
   methods: {
     getCurrentWeatherDay(allDaysArr) {
-      console.log(allDaysArr)
+      let today = new Date();
+      let day = String(today.getDate()).padStart(2, "0");
+      let month = String(today.getMonth() + 1).padStart(2, "0");
+      let year = today.getFullYear();
+
+      today = `${year}-${month}-${day}`;
+      this.date = today;
+
+      this.setTime();
+
+      let currentWeatherDay = [];
+
+      allDaysArr.forEach((element) => {
+        console.log(element.dt_txt.slice(0, 10));
+        if (element.dt_txt.slice(0, 10) === today) {
+          currentWeatherDay.push(element);
+        }
+      });
+
+      // console.log(today, "today");
+      // console.log(currentWeatherDay, 'currentWeatherDay');
+      // console.log(allDaysArr, "allDaysArr");
+    },
+    setTime() {
+      let today = new Date();
+      let hour = today.getHours();
+      let minutes = today.getMinutes();
+      let sec = today.getSeconds();
+
+      let time = `${hour}:${minutes}:${sec}`;
+      // console.log(time);
+
+      this.time = {
+        hour,
+        minutes,
+      };
+
+      setInterval(() => {
+        sec = sec + 1;
+        if (sec == 60) {
+          this.time.minutes = this.time.minutes + 1;
+          sec = 0;
+          if (this.time.minutes == 60) {
+            this.time.minutes = 0;
+            this.time.hour = this.time.hour + 1;
+            if (this.time.hour == 24) {
+              clearInterval();
+              this.time.hour = 0;
+              this.getCurrentWeatherDay();
+            }
+          }
+        }
+      }, 1000);
     },
   },
   components: {
@@ -47,6 +99,11 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
